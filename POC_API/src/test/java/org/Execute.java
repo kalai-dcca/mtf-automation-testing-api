141a4bb0aed3d8b590556b98fd387cbdfc46b80c
aTurukDCCA <aturuk@dcca.com>
2024-12-06 12:48:51 -0500

    feature/ATURUK
    added fake file function

--- POC_API/src/test/java/org/Execute.java
+++ POC_API/src/test/java/org/Execute.java
@@ -2,22 +2,31 @@ package org;
 
 import org.utilities.FileManager;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Execute {
     public static void main(String[] args) {
         FileManager.createFileCopy("confirmClone.txt",
                 "confirmCloneDup.txt");
 
-        Map<String,String> data =  FileManager.getFileDirectory("mtf-automation-testing-api");
+        //Map<String,String> data =  FileManager.getFileDirectory("mtf-automation-testing-api");
 
-        FileManager.createFile("mtf-automation-testing-api","testFile",".pdf");
+        //FileManager.createFile("mtf-automation-testing-api","testFile",".pdf");
 
-        StringBuilder sb = FileManager.readFile("confirmClone.txt");
+        //StringBuilder sb = FileManager.readFile("confirmClone.txt");
 
-        List<String> list = FileManager.readFileLines("confirmClone.txt");
+        //Map<Integer,List<String>> dataCsv = FileManager.getDataFromCsv("list.csv",",");
 
-        Map<Integer,List<String>> dataCsv = FileManager.getDataFromCsv("list.csv",",");
+        List<String> list = FileManager.readFileLines("PDEData_20240729132923-fake.txt");
+
+        Map<String,List<String>> fakeFileMap = new HashMap<>();
+
+        for(String each : list){
+            String[] s = each.trim().split(" ");
+            List<String> dataList = Arrays.stream(s).collect(Collectors.toList()).stream().filter(p -> Objects.nonNull(p) && !p.trim().isEmpty()).collect(Collectors.toList());
+            dataList.remove(s[0]);
+            fakeFileMap.put(s[0],dataList);
+        }
     }
 }
