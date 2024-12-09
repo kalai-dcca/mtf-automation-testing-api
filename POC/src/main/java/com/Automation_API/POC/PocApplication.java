096796d202b37f79625816083a10b8a7141fa27b
aTurukDCCA <aturuk@dcca.com>
2024-12-09 16:40:40 -0500

    feature/POC-Spring-Boot
    Added Spring Boot Project

---
+++ POC/src/main/java/com/Automation_API/POC/PocApplication.java
@@ -0,0 +1,13 @@
+package com.Automation_API.POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApplication.class, args);
+	}
+
+}
